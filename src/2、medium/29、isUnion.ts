8f0ccb6f1146e8a2390213a7f41d19e6975f7cd9
YangLG-7 <1219649500@qq.com>
2022-02-24 09:55:18 +0800

    feat(perf):medium-IsUnion

--- src/2、medium/29、isUnion.ts
+++ src/2、medium/29、isUnion.ts
@@ -10,14 +10,15 @@
 // 如果T 是联合类型则T是单一的数据
 // 因为T 是联合类型，联合类型在extends会满足分配律，而U 则是联合类型
 
-type IsUnion<T, U = T> = T extends T
+type IsUnion<T, U = T> = [T] extends never[]
+  ? false
+  : T extends T
   ? [Exclude<U, T>] extends never[]
     ? false
     : true
   : false;
 // Exclude<string, string>  => never
 // 这里需要是数组的原因是因为
-
 // never 不会走 extends。因为 never 在左侧直接返回never
 
 /* _____________ Test Cases _____________ */
@@ -25,6 +26,7 @@ import { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
   Expect<Equal<IsUnion<string>, false>>,
+  Expect<Equal<IsUnion<never>, false>>,
   Expect<Equal<IsUnion<string | number>, true>>,
   Expect<Equal<IsUnion<"a" | "b" | "c" | "d">, true>>,
   Expect<Equal<IsUnion<undefined | null | void | "">, true>>,
