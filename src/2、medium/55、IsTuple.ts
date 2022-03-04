5c189285d1820ddc0241757b1d99cc6b9e432b0d
YangLG-7 <1219649500@qq.com>
2022-03-04 17:03:30 +0800

    feat(challenge):medium-IsTuple

---
+++ src/2、medium/55、IsTuple.ts
@@ -0,0 +1,28 @@
+/*
+ * @description IsTuple
+ * @link https://github.com/type-challenges/type-challenges/blob/master/questions/4484-medium-istuple/README.md
+ */
+
+/* _____________ Your Code Here _____________ */
+// type IsTuple<T> = T extends readonly [infer F, ...infer R]
+//   ? true
+//   : T extends []
+//   ? true
+//   : false;
+
+type IsTuple<T> = T extends readonly any[]
+  ? number extends T["length"] // 这里不能写成 T["length"] extends number 因为number是大类型 1234这种数组也是extends number
+    ? false
+    : true
+  : false;
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect, ExpectFalse, NotEqual } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<IsTuple<[]>, true>>,
+  Expect<Equal<IsTuple<[number]>, true>>,
+  Expect<Equal<IsTuple<readonly [1]>, true>>,
+  Expect<Equal<IsTuple<{ length: 1 }>, false>>,
+  Expect<Equal<IsTuple<number[]>, false>> // 这个里面并没有内容。 T["length"] => number
+];
