e2cd5d19e2e30dc024595b4711d1a9567b1321e8
YangLG-7 <1219649500@qq.com>
2022-03-04 17:05:17 +0800

    feat(challenge):medium-IsTuple

--- src/2、medium/55、IsTuple.ts
+++ src/2、medium/55、IsTuple.ts
@@ -10,8 +10,17 @@
 //   ? true
 //   : false;
 
-type IsTuple<T> = T extends readonly any[]
-  ? number extends T["length"] // 这里不能写成 T["length"] extends number 因为number是大类型 1234这种数组也是extends number
+// type IsTuple<T> = T extends readonly any[]
+//   ? number extends T["length"] // 这里不能写成 T["length"] extends number 因为number是大类型 1234这种数组也是extends number
+//     ? false
+//     : true
+//   : false;
+
+// 如果需要解决never的问题则需要
+type IsTuple<T> = [T] extends [never]
+  ? false
+  : T extends readonly any[]
+  ? number extends T["length"]
     ? false
     : true
   : false;
@@ -24,5 +33,6 @@ type cases = [
   Expect<Equal<IsTuple<[number]>, true>>,
   Expect<Equal<IsTuple<readonly [1]>, true>>,
   Expect<Equal<IsTuple<{ length: 1 }>, false>>,
-  Expect<Equal<IsTuple<number[]>, false>> // 这个里面并没有内容。 T["length"] => number
+  Expect<Equal<IsTuple<number[]>, false>>, // 这个里面并没有内容。 T["length"] => number
+  Expect<Equal<IsTuple<never>, false>>
 ];
